9052dc15b41dcba493fedde540e5c52c2391fe4f
sahilgoel <sahil@Sahils-MacBook-Pro.local>
2015-12-10 18:37:09 -0500

    Support for adding reservations

--- helloworld/main.py
+++ helloworld/main.py
@@ -2,11 +2,13 @@ import os
 import urllib
 from google.appengine.api import users
 from google.appengine.ext import ndb
+#from google.appengine.api import datastore
 import jinja2
 import webapp2
 import datetime
 import time
 import uuid
+import copy
 from jinja2 import Environment
 
 JINJA_ENVIRONMENT = jinja2.Environment(
@@ -42,10 +44,11 @@ class Reservations(ndb.Model):
     resourceUid = ndb.StringProperty(indexed=False)
     reservedBy = ndb.StringProperty(indexed=True)
     startTime = ndb.DateTimeProperty(indexed=True)
-    endTime = ndb.DateTimeProperty(indexed=False)
+    duration = ndb.StringProperty(indexed=False)
     uid = ndb.StringProperty(indexed=True)
 
-def getReservations(userId):
+
+def getCurrentTimeObject():
     currentTime = datetime.datetime.now()
     # To convert in EST/EDT
     delta = datetime.timedelta(hours = 5)
@@ -54,10 +57,16 @@ def getReservations(userId):
     minute = currentTime.minute
     currenTimeString = str(hour)+':'+str(minute)
     currentTimeObject = datetime.datetime.strptime(currenTimeString,'%H:%M')
+    return currentTimeObject
+
+def getReservations(userId):
+    currentTimeObject = getCurrentTimeObject()
     reservations_query = Reservations.query(ancestor=reservation_key(userId))
     reservations_query = reservations_query.order(Reservations.startTime)    
     reservations = reservations_query.fetch()
-    reservations = [ r for r in reservations if r.endTime >= currentTimeObject ]   
+    print reservations
+    print currentTimeObject
+    reservations = [ r for r in reservations if r.startTime + datetime.timedelta(minutes = int(r.duration)) >= currentTimeObject ] 
     return reservations
 
 class TimeSlot(ndb.Model):
@@ -73,12 +82,23 @@ class Resource(ndb.Model):
     lastReservation = ndb.DateTimeProperty(indexed=True)
     uid = ndb.StringProperty(indexed=True)
 
+def timeCompare(t1, t2):
+    if t1.startTime > t2.startTime:
+        return 1
+    if t1.startTime == t2.startTime:
+        return 0
+    if t1.startTime < t2.startTime:
+        return -1
+
 def processAvailabilities(availabilities):
     result = ''
+    if availabilities is None:
+        return result
+    availabilities.sort(timeCompare)
     for availability in availabilities:
         startTime = availability.startTime.strftime('%H:%M')
         endTime = availability.endTime.strftime('%H:%M')
-        timeslot = startTime+"-" +endTime + " "
+        timeslot = startTime+"-" +endTime + "     "
         result = result + timeslot
     return result
 
@@ -126,6 +146,41 @@ def checkDuration(duration):
     duration = int(duration)
     if duration <= 0:
         return True
+
+def checkAndModifyAvailability(resource, startTime, duration):
+    startTime = datetime.datetime.strptime(startTime,'%H:%M')
+    delta = datetime.timedelta(minutes = int(duration))
+    endTime = startTime + delta
+    result = None
+    for availability in resource.availability:
+        if availability.startTime <= startTime and availability.endTime >= endTime:
+            result = availability
+            break
+    # Resource is not available
+    if result is None:
+        return True
+    resource.availability.remove(result)
+    resultFirst = result
+    resultSecond = copy.deepcopy(resultFirst)
+    resultFirst.endTime = startTime - datetime.timedelta(minutes = 1)
+    resultSecond.startTime = endTime
+    resource.availability.append(resultFirst)
+    resource.availability.append(resultSecond)
+    resource.put()
+
+def addReservation(uid, startTime, duration, resource):
+    reservation = Reservations(parent=reservation_key(users.get_current_user().user_id()))
+    reservation.reservedBy = str(users.get_current_user().email())
+    reservation.startTime = datetime.datetime.strptime(startTime,'%H:%M')
+    reservation.duration = duration
+    reservation.resourceName = resource.name
+    reservation.resourceUid = uid
+    reservation.uid = str(uuid.uuid4())
+    reservation.put()
+    resource.reservations.append(reservation)
+    resource.lastReservation = datetime.datetime.now()
+    resource.put()
+
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
@@ -180,6 +235,11 @@ class AddReservation(webapp2.RequestHandler):
             error = checkDuration(duration)
             if error:
                 errorMsg = 'Duration should be a positive number'
+        if errorMsg is None:
+            resource = Resource.query(Resource.uid == uid).get()
+            error = checkAndModifyAvailability(resource, startTime, duration)
+            if error:
+                errorMsg = 'Resource is not available during that time'
         if error:
             template_values = {
                 'uid' : uid,
@@ -189,11 +249,22 @@ class AddReservation(webapp2.RequestHandler):
             }
             template = JINJA_ENVIRONMENT.get_template('addReservation.html')
             self.response.write(template.render(template_values))
+            return
+
+        # If everything fine, then add reservation in datastore
+        resource = Resource.query(Resource.uid == uid).get()
+        addReservation(uid, startTime, duration, resource)
+        self.redirect('/')
+
+
 
 class ResourceMain(webapp2.RequestHandler):
     def get(self):
         resourceUid = self.request.GET['uid']
         resource = getResourceFromUid(resourceUid)
+        currentTimeObject = getCurrentTimeObject()
+        resource.reservations = [ r for r in resource.reservations if 
+            r.startTime + datetime.timedelta(minutes = int(r.duration)) >= currentTimeObject ]
         currentUser = str(users.get_current_user().email())
         template_values = {
             'resource':resource,
